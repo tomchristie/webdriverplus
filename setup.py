3eda769eed39b5322732e97f977d1fe436fd2a67
Tom Christie <tom@tomchristie.com>
2012-06-02 17:02:43 +0100

    Added 'setup publish' command

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 
 import re
 import os
+import sys
 
 PACKAGE = 'webdriverplus'
 
@@ -12,6 +13,13 @@ base_dir = os.path.dirname(__file__)
 module = os.path.join(base_dir, PACKAGE, '__init__.py')
 version = eval(re.search('VERSION = (.*)', open(module).read()).group(1))
 version_str = '%d.%d.%d' % (version[0], version[1], version[2])
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %s -m 'version %s'" % (version, version)
+    print "  git push --tags"
+    sys.exit()
 
 setup(
     name=PACKAGE,
